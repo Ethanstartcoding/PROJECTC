cb61d402fdf021953be9a89e5b2c59dafa66974b
Trinh Minh Hieu <trinhminhhieu18122008@gmail.com>
2026-09-24 23:45:27 +0700

    up

---
+++ day2.c/problem2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int seat[3][4] = {{0,1,0,1},{1,0,0,0},{0,0,1,1}};
+    int i,j;
+    int avail_count = 0;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 4; j++){
+            if (seat[i][j] == 0){
+                printf("Seats are available at row %d, column %d\n",i,j);
+                avail_count++;
+            } else if (seat[i][j] == 1){
+                continue;
+            }
+        }
+    }
+    printf("There are total %d seats available",avail_count);
+}
